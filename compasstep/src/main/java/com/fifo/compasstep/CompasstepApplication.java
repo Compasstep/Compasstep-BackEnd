6e890df535eb03ec99d0edb30419389822a9ec0d
seominjae1 <tjalswoo@naver.com>
2025-09-19 13:22:31 +0900

    초기 설정 파일 추가

---
+++ compasstep/src/main/java/com/fifo/compasstep/CompasstepApplication.java
@@ -0,0 +1,13 @@
+package com.fifo.compasstep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompasstepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompasstepApplication.class, args);
+	}
+
+}
---
+++ compasstep/src/test/java/com/fifo/compasstep/CompasstepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fifo.compasstep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompasstepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
